dfc7187f00a98a5e6eb0e29a49106ca872429924
SaurabhGavali1435 <saurabhgavali71@gmail.com>
2025-01-06 16:27:30 +0530

    Update GitDemo.java: added one line

--- GitDemo.java
+++ GitDemo.java
@@ -3,5 +3,6 @@ class GitDemo {
 
     public static void main(String[] args) {
         System.out.println("This is practice for github");
+        System.out.println("This is first change i make");
     }
 }
